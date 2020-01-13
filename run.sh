b25829938d279d106fe688e3b3275b73fc792e9a
Florian Huber <florian@freshx.de>
2020-01-13 15:26:39 +0100

    add run script

---
+++ run.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+source /usr/local/benji/activate
+# virtualenv is now active.
+#
+DATE=$(date +"%Y-%m-%d_%H-%M-%S")
+cd /data/backup/pvesnapbackup && python script.py | tee /var/log/pvesnapbackup_$DATE.log
+exit 0
+
