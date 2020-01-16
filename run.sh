9ae1e58ba88b2b19a939e07984d831d0985a6bb3
Florian Huber <florian@freshx.de>
2020-01-16 09:20:42 +0100

    fix f strings in logging

--- run.sh
+++ run.sh
@@ -5,17 +5,22 @@ set -eo pipefail
 source /usr/local/benji/bin/activate
 # virtualenv is now active.
 #
+LOGDIR="/var/log/pvesnapbackup"
+logger "$0 Setting up log directory $LOGDIR"
+mkdir -p $LOGDIR
+logger "$0 Cleanup old logs in $LOGDIR"
+find $LOGDIR -type f -name 'pvesnapbackup_*' -mtime +14 -exec rm {} \;
 DATE=$(date +"%Y-%m-%d_%H-%M-%S")
-LOGFILE=/var/log/pvesnapbackup_$DATE.log
-echo "Starting backup expiration" | tee -a $LOGFILE
-benji enforce latest3,days7,weeks4,months12 | tee -a $LOGFILE
-cd /data/backup/pvesnapbackup && python -u backupWrapper.py | tee -a $LOGFILE
-echo "Starting deep scrubbing of 14 % of all images" | tee -a $LOGFILE
-benji batch-deep-scrub --version-percentage 14 | tee -a $LOGFILE 
+LOGFILE=/var/log/pvesnapbackup/pvesnapbackup_$DATE.log
+logger "$0 Sending logs to $LOGFILE"
+echo "Starting backup expiration" |& tee -a $LOGFILE
+benji enforce latest3,days7,weeks4,months12 |& tee -a $LOGFILE
+cd /data/backup/pvesnapbackup && python -u backupWrapper.py |& tee -a $LOGFILE
+echo "Starting deep scrubbing of 14 % of all images" |& tee -a $LOGFILE
+benji batch-deep-scrub --version-percentage 14 |& tee -a $LOGFILE 
 INVALID=$(benji -m --log-level ERROR ls | jq -r '.versions[] | select(.status|test("invalid"))| .uid, .status' | wc -l)
 if [ $INVALID -ne 0 ]; then 
-    echo "One or more backups are invalid! Manual intervention needed!" | tee -a $LOGFILE
+    echo "One or more backups are invalid! Manual intervention needed!" |& tee -a $LOGFILE
     benji ls 'status != "valid"' | mail -s "Invalid Benji backups found! Please check!" florian@freshx.de
 fi
 exit 0
-
