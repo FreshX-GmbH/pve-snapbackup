0edae055a7a9337fd0f3c378a484e4c5b9cf5e92
Florian Huber <florian@freshx.de>
2020-01-21 12:06:16 +0100

    reduced deep-scrubbing

--- run.sh
+++ run.sh
@@ -5,6 +5,9 @@ set -eo pipefail
 source /usr/local/benji/bin/activate
 # virtualenv is now active.
 #
+TMPDIR=/tmp/benji
+TMPFILE=${TMPDIR}/deep_scrub
+SCRUB_MAXAGE=86400
 LOGDIR="/var/log/pvesnapbackup"
 logger "$0 Setting up log directory $LOGDIR"
 mkdir -p $LOGDIR
@@ -16,11 +19,25 @@ logger "$0 Sending logs to $LOGFILE"
 echo "Starting backup expiration" | tee -a $LOGFILE
 benji enforce latest3,days7,weeks4,months12 | tee -a $LOGFILE
 cd /data/backup/pvesnapbackup && python -u backupWrapper.py |& tee -a $LOGFILE
-echo "Starting deep scrubbing of 14 % of all images" |& tee -a $LOGFILE
-benji batch-deep-scrub --version-percentage 14 | tee -a $LOGFILE 
-INVALID=$(benji -m --log-level ERROR ls | jq -r '.versions[] | select(.status|test("invalid"))| .uid, .status' | wc -l)
-if [ $INVALID -ne 0 ]; then 
-    echo "One or more backups are invalid! Manual intervention needed!" |& tee -a $LOGFILE
-    benji ls 'status != "valid"' | mail -s "Invalid Benji backups found! Please check!" florian@freshx.de
+# Start deep scrubbing if it did not run for more than 24 hours
+if [ ! -d /tmp/benji ]; then
+    mkdir $TMPDIR
+    touch $TMPFILE
 fi
+if [ ! -e /tmp/benji/deep_scrub ]; then
+    touch $TMPFILE
+fi
+MTIME=$(stat -c %Y $TMPFILE)
+NOW=$(date +%s)
+LASTSCRUB=$(( $NOW-$MTIME ))
+if [ $LASTSCRUB -gt $SCRUB_MAXAGE ]; then
+    echo "Starting deep scrubbing of 14 % of all images" |& tee -a $LOGFILE
+    benji batch-deep-scrub --version-percentage 14 | tee -a $LOGFILE 
+    INVALID=$(benji -m --log-level ERROR ls | jq -r '.versions[] | select(.status|test("invalid"))| .uid, .status' | wc -l)
+    if [ $INVALID -ne 0 ]; then 
+        echo "One or more backups are invalid! Manual intervention needed!" |& tee -a $LOGFILE
+        benji ls 'status != "valid"' | mail -s "Invalid Benji backups found! Please check!" florian@freshx.de
+    fi
+fi
+
 exit 0
