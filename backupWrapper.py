e238547d28e20672f3798d0160b55f38d9b6f915
Florian Huber <florian@freshx.de>
2020-02-06 12:52:10 +0100

    exclude backup=0 disks from backup

--- backupWrapper.py
+++ backupWrapper.py
@@ -315,7 +315,9 @@ for node in toBackupDict:
                 __snapConf = getSnapConfig(node, vmid, __lastSnap['name'])
                 for key,value in __snapConf.items():
                     if re.match(r'scsi\d',key):
-                        if re.search('backup=0',value[0]) == None:
+                        if re.search('backup=0',value):
+                            logging.info(f'Skipping backup for disk {key} of VM {vmid}, because backup=0 is set.')
+                        else:
                             __snapDisks.append(re.sub(':','/',(re.split(',',value)[0])))
                 logging.info(f'Running backup for disks: {__snapDisks}')
             # benji backup 
