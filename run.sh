556cef3addc8f815a2069413c05227e7ec9e27e8
Florian Huber <florian@freshx.de>
2020-04-29 12:58:57 +0200

    fix timeout issues

--- run.sh
+++ run.sh
@@ -14,7 +14,7 @@ SCRUB_MAXAGE=86400
 LOGDIR="/var/log/pvesnapbackup"
 SQLITE_DB="/data/backup/benji/db/benji.sqlite"
 SQLITE_BIN="/usr/bin/sqlite3"
-BENJI_RETENTION="latest3,days7,weeks4,months12"
+BENJI_RETENTION="latest3,days7,weeks4"
 logger "$0 Setting up log directory $LOGDIR"
 mkdir -p $LOGDIR
 logger "$0 Cleanup old logs in $LOGDIR"
@@ -27,23 +27,24 @@ function cleanup {
 }
 
 logger "$0 Sending logs to $LOGFILE"
-echo "Checking for locks in benji Database" | tee -a $LOGFILE
+echo "$(date +'%Y-%m-%d %H:%M:%S') Checking for locks in benji Database" | tee -a $LOGFILE
 if [ "$(echo 'select * from locks;' | $SQLITE_BIN $SQLITE_DB)" != "" ]; then
-    echo "backing up database before modifying it" | tee -a $LOGFILE
+	echo "$(date +'%Y-%m-%d %H:%M:%S') backing up database before modifying it" | tee -a $LOGFILE
     cp -v $SQLITE_DB $SQLITE_DB.bak_$(date +%s) |& tee -a $LOGFILE
     TRY=1
     while [ "$(echo 'select * from locks;' | $SQLITE_BIN $SQLITE_DB)" != "" ]; do
-        echo "trying to delete locks from database" | tee -a $LOGFILE
+	    echo "$(date +'%Y-%m-%d %H:%M:%S') trying to delete locks from database" | tee -a $LOGFILE
         echo "DELETE FROM locks WHERE reason = 'NBD';" | $SQLITE_BIN $SQLITE_DB |& tee -a $LOGFILE
         ((TRY++))
         if [ "$TRY" -gt 5 ]; then
-            echo "Failed to unlock database.. giving up" | tee -a $LOGFILE 
+		echo "$(date +'%Y-%m-%d %H:%M:%S') Failed to unlock database.. giving up" | tee -a $LOGFILE 
             exit 1
         fi
     done
 fi
-echo "Starting backup expiration" | tee -a $LOGFILE
-benji enforce ${BENJI_RETENTION} | tee -a $LOGFILE
+echo "$(date +'%Y-%m-%d %H:%M:%S') Starting backup expiration" |& tee -a $LOGFILE
+/usr/local/benji/bin/benji enforce ${BENJI_RETENTION} |& tee -a $LOGFILE
+echo "$(date +'%Y-%m-%d %H:%M:%S') Backup expiration done" |& tee -a $LOGFILE
 cd /data/backup/pvesnapbackup && python -u backupWrapper.py |& tee -a $LOGFILE
 
 # Check logfile for initial backups and inform admins 
@@ -62,11 +63,11 @@ NOW=$(date +%s)
 LASTSCRUB=$(( $NOW-$MTIME ))
 if [ $LASTSCRUB -gt $SCRUB_MAXAGE ]; then
     echo "Starting deep scrubbing of 14 % of all images" |& tee -a $LOGFILE
-    benji batch-deep-scrub --version-percentage 14 | tee -a $LOGFILE && touch $TMPFILE
+    /usr/local/benji/bin/benji batch-deep-scrub --version-percentage 14 |& tee -a $LOGFILE && touch $TMPFILE
     INVALID=$(benji -m --log-level ERROR ls | jq -r '.versions[] | select(.status|test("invalid"))| .uid, .status' | wc -l)
     if [ $INVALID -ne 0 ]; then 
         echo "One or more backups are invalid! Manual intervention needed!" |& tee -a $LOGFILE
-        benji ls 'status != "valid"' | mail -s "Invalid Benji backups found! Please check!" $RECEIPIENTS
+        /usr/local/benji/bin/benji ls 'status != "valid"' | mail -s "Invalid Benji backups found! Please check!" $RECEIPIENTS
     fi
 fi
 
